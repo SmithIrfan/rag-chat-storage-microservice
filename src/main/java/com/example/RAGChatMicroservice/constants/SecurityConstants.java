38cf7f00bdc058a7e6ada6756e64eaf17fbd368c
Irfan Ali <irfanaliahangar@gmail.com>
2025-12-14 19:50:19 +0530

    key name changes

--- src/main/java/com/example/RAGChatMicroservice/constants/SecurityConstants.java
+++ src/main/java/com/example/RAGChatMicroservice/constants/SecurityConstants.java
@@ -10,7 +10,7 @@ public final class SecurityConstants {
      * Name of the HTTP header used for API key authentication.
      */
     public static final String API_KEY_SCHEME = "ApiKeyAuth";
-    public static final String HEADER_API_KEY = "X-API-KEY";
+    public static final String HEADER_API_KEY = "API-KEY";
     public static final String ERROR_INVALID_API_KEY = "Invalid or missing API key";
 
     public static final String PROP_SECURITY_API_KEY = "security.api-key";
